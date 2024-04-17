97d5decd759ecdb7dfa778612fb7a4c8b52ec5be
kimziyeon <aa40254037@gmail.com>
2024-04-17 22:31:14 +0900

    20240417-2

--- src/comp/UIUX/AddNote.tsx
+++ src/comp/UIUX/AddNote.tsx
@@ -5,91 +5,91 @@ import { useState } from 'react';
 import { useStore } from '../store/note_store';
 
 
-function AddNote({setAddNote}:any) {
+function AddNote({ setAddNote }: any) {
 
-    let {data2, dataFetch2} = useStore();
+    let { data2, dataFetch2 } = useStore();
 
     const today = new Date();
-    let todayFormat = format(today,"yyyy.MM.dd");
+    let todayFormat = format(today, "yyyy.MM.dd");
     let [title, setTitle] = useState('');
     let [contents, setContents] = useState('');
-    let [colorNum,setColorNum] = useState(1);
-    let [color, setColor] = useState('#4385F5');
-    let colorPalette =[
-        '#4385F5','#34A853','#FCBC05','#E8463B'
+    let [colorNum, setColorNum] = useState(1);
+    let [color, setColor] = useState('#D2D2D4');
+    let colorPalette = [
+        '#D2D2D4', '#4385F5', '#34A853', '#FCBC05', '#E8463B'
     ]
-    let [bookMark, setBookMark]= useState(false);
-    
-    const offClick=()=>{
+    let [bookMark, setBookMark] = useState(false);
+
+    const offClick = () => {
         setAddNote(false);
     }
 
     //데이터 추가
-    const dataAdd = ()=>{
+    const dataAdd = () => {
         let dataValue = {
-            'id':Date.now(),
+            'id': Date.now(),
             'title': title,
             'contents': contents,
-            'date':todayFormat,
-            'color' : color,
-            'bookmark':String(!bookMark),
+            'date': todayFormat,
+            'color': color,
+            'bookmark': String(bookMark),
             'url': ''
         }
-        dataFetch2('post',dataValue)
+        dataFetch2('post', dataValue)
 
         setAddNote(false);
     }
 
-    const submit=(e:any)=>{
+    const submit = (e: any) => {
         e.preventDefault();
     }
 
-    const colorClick=()=>{
+    const colorClick = () => {
         // console.log('click'+colorPalette[0])
         // console.log(colorPalette[colorNum])
 
         setColor(colorPalette[colorNum])
-        setColorNum(colorNum++)
-        if(colorNum == 4){
+        setColorNum(colorNum + 1)
+        if (colorNum == 5) {
             setColorNum(0)
         }
     }
-    
-    const bookMarkClick=()=>{
+
+    const bookMarkClick = () => {
         setBookMark(!bookMark)
     }
 
 
-  return (
-    <>
-    <div className='addBack' onClick={offClick}></div>
+    return (
+        <>
+            <div className='addBack' onClick={offClick}></div>
 
-    <article className='addMemo'>
-        <div className='addMemoC1'>
-            <div className='more'>
-            <img src="/images/more_gray.png" alt="more_gray" />
-            </div>
-            <p onClick={dataAdd}style={{color:color}}>저장</p>
-        </div>
-        <div className='addMemoC2'>
-            
-            <form onSubmit={submit}>
-                <input type="text" placeholder='제목을 입력하세요.' onChange={(e)=>{setTitle(e.target.value)}}/>
-                <textarea name="내용" id="" placeholder='내용을 입력하세요.' onChange={(e)=>{setContents(e.target.value)}}></textarea>
-            </form>
+            <article className='addMemo'>
+                <div className='addMemoC1'>
+                    <div className='more'>
+                        <img src="/images/more_gray.png" alt="more_gray" />
+                    </div>
+                    <p onClick={dataAdd} style={{ color: color }}>저장</p>
+                </div>
+                <div className='addMemoC2'>
 
-            <div className='addMemoC3' style={{backgroundColor:color}}>
-                <div className='colorPalette' onClick={()=>{colorClick()}}>
-                    <img src="/images/colorp.png" alt="colorp" />
+                    <form onSubmit={submit}>
+                        <input type="text" placeholder='제목을 입력하세요.' onChange={(e) => { setTitle(e.target.value) }} />
+                        <textarea name="내용" id="" placeholder='내용을 입력하세요.' onChange={(e) => { setContents(e.target.value) }}></textarea>
+                    </form>
+
+                    <div className='addMemoC3' style={{ backgroundColor: color }}>
+                        <div className='colorPalette' onClick={() => { colorClick() }}>
+                            <img src="/images/colorp.png" alt="colorp" />
+                        </div>
+                        <div className='bookmark' onClick={() => { bookMarkClick() }}>
+                            <img src={bookMark == true ? "/images/bookmark_large_on.png" : "/images/bookmark_large_off.png"} alt="bookmarkLarge" />
+                        </div>
+                    </div>
                 </div>
-                <div className='bookmark' onClick={()=>{bookMarkClick()}}>
-                    <img src={bookMark == true ? "/images/bookmark_large_on.png":"/images/bookmark_large_off.png"} alt="bookmarkLarge" />
-                </div>
-            </div>
-        </div>
-</article>
-</>
-  );
+            </article>
+        </>
+    );
 }
 
 export default AddNote;
--- src/comp/UIUX/NoteComp.tsx
+++ src/comp/UIUX/NoteComp.tsx
@@ -2,29 +2,29 @@
 
 import React from 'react';
 import '../style/note.scss';
-import {useEffect, useState, useRef} from 'react';
+import { useEffect, useState, useRef } from 'react';
 
-function NoteComp({obj}:any) {
+function NoteComp({ obj }: any) {
     console.log(obj)
 
     return (
-    
+
         <div className='mapMemo'>
             <div className='mContents'>
                 <b>{obj.title}</b>
                 <p>{obj.contents}</p>
             </div>
-            
-            <div className='memoDate'>
+
+            <div className='memoDate' style={{ backgroundColor: obj.color }}>
                 <span>{obj.date}</span>
             </div>
 
             <div className='bookmark'>
-                <img src={obj.complete=='true'?"/images/bookmark_on.png":"/images/bookmark_off.png"} alt="bookmark" />
+                <img src={obj.bookmark == 'true' ? "/images/bookmark_on.png" : "/images/bookmark_off.png"} alt="bookmark" />
             </div>
         </div>
 
-        
+
     );
 }
 
