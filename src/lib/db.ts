de0401361bcfa11c6fff79c9890a0268ec1eabad
kimziyeon <aa40254037@gmail.com>
2024-04-25 10:59:37 +0900

    20240425

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,22 +2,32 @@ import { createPool } from 'mysql2'
 
 let mysql = require('mysql2');
 const pool = {
-    host:  process.env.HOST ,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.PORT
+    host: process.env.NEXT_PUBLIC_HOST,
+    user: process.env.NEXT_PUBLIC_USER,
+    password: process.env.NEXT_PUBLIC_PASSWORD,
+    database: process.env.NEXT_PUBLIC_DATABASE,
+    port: process.env.NEXT_PUBLIC_PORT
 }
 
-export const queryExecute = async (query:any,values:any)=>{
-        const connection = mysql.createConnection(pool);
-        connection.connect();
-        return await new Promise((resolve,reject)=>{
-            connection.query(query,values, function(error:any, results:any, fields:any){
-                if(error) console.log(error);
-                else console.log('Connected to db...!')
-                resolve(results);
-                connection.end();
-            })
+export const queryExecute = async (query: any, values: any) => {
+
+    // console.log(
+    //     {
+    //         host: process.env.NEXT_PUBLIC_HOST,
+    //         user: process.env.NEXT_PUBLIC_USER,
+    //         password: process.env.NEXT_PUBLIC_PASSWORD,
+    //         database: process.env.NEXT_PUBLIC_DATABASE,
+    //         port: process.env.NEXT_PUBLIC_PORT
+    //     }
+    // )
+    const connection = mysql.createConnection(pool);
+    connection.connect();
+    return await new Promise((resolve, reject) => {
+        connection.query(query, values, function (error: any, results: any, fields: any) {
+            if (error) console.log(error);
+            else console.log('Connected to db...!')
+            resolve(results);
+            connection.end();
         })
-    }
+    })
+}
